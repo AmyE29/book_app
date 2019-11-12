e2097e867b08bc382248ae731fcfc745b7ac4150
Amy Evans <amykarline@yahoo.com>
2019-11-12 10:35:42 -0800

    got Hello World and color to appear

---
+++ server.js
@@ -0,0 +1,25 @@
+"use strict";
+
+require('dotenv').config();
+
+const express=require('express');
+// const bodyParser=require('body-parser');
+// const cors=require('cors');
+
+const app = express();
+// app.use(bodyParser());
+// app.use(cors());
+const PORT = process.env.PORT;
+
+app.use( express.static( './public' ));
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+    response.render('pages/index');
+  })
+
+
+
+app.listen(PORT, () => {
+      console.log(`listening on :${PORT}`);
+    })
