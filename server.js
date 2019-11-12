bfe6fe12a8b58b934f30d42990f9bd850a35ce69
Chase Tucker <tuck8106@gmail.com>
2019-11-12 12:48:05 -0800

    getting there

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
-"use strict";
+'use strict';
 
+//////////////////////////////////////////////////
+// Configs
+//////////////////////////////////////////////////
 require('dotenv').config();
 
+//////////////////////////////////////////////////
+// Dependencies
+//////////////////////////////////////////////////
 const express=require('express');
-// const bodyParser=require('body-parser');
-// const cors=require('cors');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const bookSearch = require('./js/bookSearch.js');
 
+//////////////////////////////////////////////////
+// Declarations
+//////////////////////////////////////////////////
 const app = express();
-// app.use(bodyParser());
-// app.use(cors());
-const PORT = process.env.PORT;
+app.use(bodyParser());
+app.use(cors());
+const PORT = process.env.PORT || 3000;
 
+//////////////////////////////////////////////////
+// Routing
+//////////////////////////////////////////////////
 app.use( express.static( './public' ));
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-    response.render('pages/index');
-  })
+  response.render('pages/index');
+});
 
-
+app.post('/search', bookSearch.volumeSearch);
 
 app.listen(PORT, () => {
-      console.log(`listening on :${PORT}`);
-    })
+  console.log(`listening on http://localhost:${PORT}`);
+})
